1f34ca8f9296fc1e1624c62ab05f1955f03bc461
Angelo Uceda <angeloucedamalca@gmail.com>
2023-05-19 22:03:39 -0500

    Se agrega la vista resumen

--- backend/login/login.php
+++ backend/login/login.php
@@ -1,51 +1,52 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="/logoFAVICON/smart.png" type="image/x-icon">
-    
+
     <link rel="stylesheet" href="/css/estilo-login.css">
     <title>login</title>
 </head>
+
 <body>
-    
-   
-<section class="left-formulario">
-    
-<form action="validar-login.php" method="post">
-
-        <div class="logo" >
-        <a  class="smart">SMART </a>
-        <img id="logo2" src="/css/logo2.png" alt="">
-        <a  class="fit"> FIT</a>
-        </div>  
-
-        <h2>Inicia sesion para <a>empezar</a> </h2> <br>
-     <p>¡Bienvenido de nuevo!</p>
-     <p>Por favor introduce tus datos para ingresar</p>
 
 
-     <label for="usuario">USUARIO</label>
-     <input class="form" type="email" name="email" placeholder="Ingrese su correo electronico" required="" >
-    
-     <label for="password">CONTRASEÑA</label>
-     <input class="form" type="password" name="contraseña"  placeholder="Ingrese su contraseña" required=""> <br>
+    <section class="left-formulario">
+
+        <form action="validar-login.php" method="post">
+
+            <div class="logo">
+                <a class="smart">SMART </a>
+                <img id="logo2" src="/css/logo2.png" alt="">
+                <a class="fit"> FIT</a>
+            </div>
+
+            <h2>Inicia sesion para <a>empezar</a> </h2> <br>
+            <p>¡Bienvenido de nuevo!</p>
+            <p>Por favor introduce tus datos para ingresar</p>
 
 
-      <div class="botones">
-     <input class="boton1" type="submit" name="ingresar" value="Ingresar" >
-     <input class="boton2" type="submit" value="Registrar" id="registrar">
-     </div>  
+            <label for="usuario">USUARIO</label>
+            <input class="form" type="email" name="email" placeholder="Ingrese su correo electronico" required="">
 
-     <p class="olvidaste"><a href="/backend/login/olvidarcontraseña.php">¿Olvidaste tu contraseña?</a></p>
+            <label for="password">CONTRASEÑA</label>
+            <input class="form" type="password" name="contraseña" placeholder="Ingrese su contraseña" required=""> <br>
 
-     </form>
+
+            <div class="botones">
+                <input class="boton1" type="submit" name="ingresar" value="Ingresar">
+                <input class="boton2" type="submit" value="Registrar" id="registrar">
+            </div>
+
+            <p class="olvidaste"><a href="/backend/login/olvidarcontraseña.php">¿Olvidaste tu contraseña?</a></p>
+
+        </form>
 
     </section>
- 
+
 
 
 
@@ -59,4 +60,5 @@
 
 
 </body>
+
 </html>
