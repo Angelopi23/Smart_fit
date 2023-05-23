a0200606df170e13f2549cad3d44c0cdc1251f40
KenyiLagos1210 <kenyilagos1210@gmail.com>
2023-05-23 01:08:43 -0500

    seleccion horas - vista

--- backend/sedes.php
+++ backend/sedes.php
@@ -69,7 +69,7 @@ if (isset($_POST['sedes'])) {
         <ul>
             <li><a class="sede" href="">SEDES</a></li>
             <li><a class="zona" href="/backend/zonasentrenamiento.php">ZONAS DE ENTRENAMIENTO</a></li>
-            <li><a class="reservar" href="">RESERVAR</a></li>
+            <li><a class="reservar" href="/backend/seleccion.php">RESERVAR</a></li>
             <li><a class="carrito" href="/backend/carrito.php">CARRITO</a></li>
           <li><a class="empieza" href=""><?php echo utf8_decode($nombre.' '.$apellido); ?></a></li>
         </ul>
